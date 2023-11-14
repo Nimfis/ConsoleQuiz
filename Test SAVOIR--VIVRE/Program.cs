3120ae0209eb3e45e2c9764ceeda0194291ffbb8
Anna Rusnak <anna.rusnak93@gmail.com>
2023-11-14 22:34:46 +0100

    Add questions and answers list

--- Test SAVOIR--VIVRE/Program.cs
+++ Test SAVOIR--VIVRE/Program.cs
@@ -15,7 +15,7 @@
             var question2 = new Question("Kto pierwszy mówi dzień dobry, osoba wchodzi do pomieszczenia czy osoby które się w pomieszczeniu znajdują?", new List<Answer>
             {
                 new Answer("pierwsze mówią osoby, które się w pomieszczeniu znajdują", true),
-                new Answer("pierwsza mówi osoba która wchodzi do pomieszczenia, nawet wtedy gdy jest przełożonym", false),
+                new Answer("pierwsza mówi osoba która wchodzi do pomieszczenia, nawet wtedy gdy jest przełożonym", false)
             }, 0);
 
             var question3 = new Question("W których z wymienionych sytuacji pozdrawiamy osoby nieznajome?", new List<Answer>
@@ -41,46 +41,41 @@
                 new Answer("kobieta – mężczyźnie", false),
                 new Answer("podwładny – przełożonemu", false),
                 new Answer("odpowiedzi a i b są prawidłowe", true)
-            }, correctOptionIndex: 2);
+            }, correctOptionIndex: 3);
 
-            var question6 = new Question("Czy prawdą jest jeśli chodzi o przedstawianie się, że?", new List<Answer>
+            var question6 = new Question("Czy będąc w grupie ściskając dłoń możemy ją uścisnąć tylko jednej osobie a resztę pominąć?", new List<Answer>
             {
-                new Answer("starszy przedstawia się młodszemu", false),
-                new Answer("kobieta przedstawia się mężczyźnie", false),
-                new Answer("żadna z tych odpowiedzi nie jest prawidłowa", true),
-                new Answer("obydwie odpowiedzi są prawidłowe", false)
-            }, correctOptionIndex: 2);
+                new Answer("w grupie należy powitać wszystkich tak samo", true),
+                new Answer("w grupie podają sobie rękę tylko ci którzy się znają", false),
+                new Answer("w grupie podają sobie rękę tylko mężczyźni", false)
+            }, correctOptionIndex: 0);
 
-            var question7 = new Question("Czy prawdą jest jeśli chodzi o przedstawianie się, że?", new List<Answer>
+            var question7 = new Question("Jaki powinien być uścisk dłoni?", new List<Answer>
             {
-                new Answer("starszy przedstawia się młodszemu", false),
-                new Answer("kobieta przedstawia się mężczyźnie", false),
-                new Answer("żadna z tych odpowiedzi nie jest prawidłowa", true),
-                new Answer("obydwie odpowiedzi są prawidłowe", false)
-            }, correctOptionIndex: 2);
+                new Answer("powinien być pewny, ale nie za mocny, w żadnym razie również zbyt słaby", true),
+                new Answer("powinien być bardzo silny", false),
+                new Answer("powinien być bardzo słabiutki", false)
+            }, correctOptionIndex: 0);
 
-            var question8 = new Question("Czy prawdą jest jeśli chodzi o przedstawianie się, że?", new List<Answer>
+            var question8 = new Question("Jak przedstawić sobie wzajemnie osoby – mężczyznę i kobietę?", new List<Answer>
             {
-                new Answer("starszy przedstawia się młodszemu", false),
-                new Answer("kobieta przedstawia się mężczyźnie", false),
-                new Answer("żadna z tych odpowiedzi nie jest prawidłowa", true),
-                new Answer("obydwie odpowiedzi są prawidłowe", false)
-            }, correctOptionIndex: 2);
+                new Answer("najpierw wymieniamy imię i nazwisko mężczyzny", true),
+                new Answer("najpierw wymieniamy imię i nazwisko kobiety", false),
+                new Answer("kolejność nie ma znaczenia", false),
+            }, correctOptionIndex: 0);
             
-            var question9 = new Question("Czy prawdą jest jeśli chodzi o przedstawianie się, że?", new List<Answer>
+            var question9 = new Question("Jak przedstawić sobie wzajemnie osoby – młodszą i starszą?", new List<Answer>
             {
-                new Answer("starszy przedstawia się młodszemu", false),
-                new Answer("kobieta przedstawia się mężczyźnie", false),
-                new Answer("żadna z tych odpowiedzi nie jest prawidłowa", true),
-                new Answer("obydwie odpowiedzi są prawidłowe", false)
-            }, correctOptionIndex: 2);
+                new Answer("starsza osoba jest przedstawiana młodszej, a następnie młodsza starszej ", false),
+                new Answer("młodsza osoba jest przedstawiana starszej, a następnie starsza młodszej", true),
+                new Answer("w relacji młodszy – starszy – kolejność jest bez różnicy", false),
+            }, correctOptionIndex: 1);
 
-            var question10 = new Question("Czy prawdą jest jeśli chodzi o przedstawianie się, że?", new List<Answer>
+            var question10 = new Question("Co robi się z pięknie ułożoną serwetką na stole, gdzie ją kładziemy?", new List<Answer>
             {
-                new Answer("starszy przedstawia się młodszemu", false),
-                new Answer("kobieta przedstawia się mężczyźnie", false),
-                new Answer("żadna z tych odpowiedzi nie jest prawidłowa", true),
-                new Answer("obydwie odpowiedzi są prawidłowe", false)
+                new Answer("za kołnierzyk", false),
+                new Answer("odkładamy na bok", false),
+                new Answer("kładziemy ją na kolanach", true),
             }, correctOptionIndex: 2);
 
 
