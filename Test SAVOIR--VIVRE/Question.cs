de7589d21f7e40f869933b28c18be77a6c27f47d
Anna Rusnak <anna.rusnak93@gmail.com>
2023-11-16 21:31:38 +0100

    Fix syntax error in Question class constructor
    - Corrected the syntax error in the Question class by fixing the constructor declarations.
    
    -Adjusted the indentation for better code formatting.
    
    -Renamed the constant for clarity.

--- Test SAVOIR--VIVRE/Question.cs
+++ Test SAVOIR--VIVRE/Question.cs
@@ -8,16 +8,19 @@ namespace Test_SAVOIR__VIVRE
 {
     internal class Question
     {
+        internal int CorrectAnswerIndex;
+
         public string Text { get; init; }
-        public List<Answer> Options { get; init; }
+        public List<Answer> Answers { get; init; }
         public int CorrectOptionIndex { get; init; }
 
-        public Question(string text, List<Answer> options, int correctOptionIndex)
+        public Question(string text, List<Answer> answers, int correctOptionIndex)
         {
             Text = text;
-            Options = options;
+            Answers = answers;
             CorrectOptionIndex = correctOptionIndex;
         }
-    }
 
+        public const ConsoleColor TextColor = ConsoleColor.DarkCyan;
+    }
 }
