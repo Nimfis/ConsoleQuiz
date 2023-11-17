aa2610005ec8eab6eb125ee419b77ae36dda197d
Anna Rusnak <anna.rusnak93@gmail.com>
2023-11-17 21:41:04 +0100

    Added class InputBlocker

---
+++ Test SAVOIR--VIVRE/InputBlocker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_SAVOIR__VIVRE
+{
+    static class InputBlocker
+    {
+        public static void BlockUserInput()
+        {
+            Console.TreatControlCAsInput = true;
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
+
+                    if (keyInfo.Key != ConsoleKey.Enter)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        break; 
+                    }
+                }
+            }
+
+            Console.TreatControlCAsInput = false;
+        }
+    }
+}
+
