395748099bb61dc6394164c5f6009f511f1b5b42
Anna Rusnak <anna.rusnak93@gmail.com>
2023-11-26 21:01:27 +0100

    Create Question and Answer entities

---
+++ Test SAVOIR--VIVRE/Persistence/Database/Entities/Answer.cs
@@ -0,0 +1,4 @@
+﻿namespace Test_SAVOIR__VIVRE.Persistence.Database.Entities
+{
+    public record Answer(Guid Id, Question Question, bool Correct, string Content);
+}
---
+++ Test SAVOIR--VIVRE/Persistence/Database/Entities/Question.cs
@@ -0,0 +1,9 @@
+﻿namespace Test_SAVOIR__VIVRE.Persistence.Database.Entities
+{
+    public record Question
+    {
+        public required Guid Id { get; init; }
+        public required string Content { get; init; }
+        public required int Points { get; init; }
+    }
+}
