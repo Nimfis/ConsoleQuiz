3f11c2c10f0b48efd875b0e459cebaac00a1429f
Anna Rusnak <anna.rusnak93@gmail.com>
2023-11-26 21:50:46 +0100

    Create IAppDbContext and its implementation

---
+++ Test SAVOIR--VIVRE/Persistence/Database/Service/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test_SAVOIR__VIVRE.Persistence.Database.Entities;
+
+namespace Test_SAVOIR__VIVRE.Persistence.Database.Service
+{
+    public class AppDbContext : DbContext, IAppDbContext
+    {
+        public DbSet<Question>  Questions { get; init; }
+        public DbSet<Answer> Answers { get; init; }
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            :base(options)
+        {
+            
+        }
+    }
+}
---
+++ Test SAVOIR--VIVRE/Persistence/Database/Service/IAppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Test_SAVOIR__VIVRE.Persistence.Database.Service
+{
+    public interface IAppDbContext
+    {
+        DbSet<Question> Questions { get; init; }
+        DbSet<Answer> Answers { get; init; }
+    }
+}
