fc133e1d75f0ba063bca755b0d35d791386468c4
Anna Rusnak <anna.rusnak93@gmail.com>
2023-11-27 19:51:16 +0100

    Create new instance of AppDbContext

--- Test SAVOIR--VIVRE/Program.cs
+++ Test SAVOIR--VIVRE/Program.cs
@@ -1,7 +1,15 @@
-﻿namespace Test_SAVOIR__VIVRE
+﻿using Microsoft.EntityFrameworkCore;
+using Test_SAVOIR__VIVRE.Persistence.Database.Service;
+
+namespace Test_SAVOIR__VIVRE
 {
-    internal class Program
+    internal static class Program
     {
+        private readonly static DbContextOptions<AppDbContext> Options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlServer("Server=localhost, 1433;Database=QuizDb;User Id=sa;Password=P@$$w0rd;")
+            .Options;
+
+        private readonly static IAppDbContext AppDbContext = new AppDbContext(Options);
         static void Main()
         {
             var quiz = new Quiz(QuestionsData.Questions);
