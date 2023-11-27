1e1721a2dfc06c3fda81568686ebefa00bf24c31
Anna Rusnak <anna.rusnak93@gmail.com>
2023-11-27 19:46:32 +0100

    Rename QABase class to QuestionsData and refactor it

--- Test SAVOIR--VIVRE/Program.cs
+++ Test SAVOIR--VIVRE/Program.cs
@@ -4,18 +4,7 @@
     {
         static void Main()
         {
-            var quiz = new Quiz();
-
-            quiz.AddQuestion(QABase.question1);
-            quiz.AddQuestion(QABase.question2);
-            quiz.AddQuestion(QABase.question3);
-            quiz.AddQuestion(QABase.question4);
-            quiz.AddQuestion(QABase.question5);
-            quiz.AddQuestion(QABase.question6);
-            quiz.AddQuestion(QABase.question7);
-            quiz.AddQuestion(QABase.question8);
-            quiz.AddQuestion(QABase.question9);
-            quiz.AddQuestion(QABase.question10);
+            var quiz = new Quiz(QuestionsData.Questions);
             quiz.StartQuiz();
         }
     }
